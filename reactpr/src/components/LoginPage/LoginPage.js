324ab9b9dd49cb39a08fedf8f042f75625957b1e
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-03-31 16:35:12 +0200

    before copy-paste

--- reactpr/src/components/LoginPage/LoginPage.js
+++ reactpr/src/components/LoginPage/LoginPage.js
@@ -1,30 +1,35 @@
 import React, { useState } from "react";
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  let navigate = useNavigate();
   const [username, setUsername] = useState("");
 
   const signInUser = (e) => {
     //e.preventDefault();
     window.localStorage.setItem("LoggedUser", JSON.stringify(username));
-    //navigate('/shopping')
+    navigate("/shopping");
   };
   return (
-    <div className={commonColumnsStyles.App}>
+    <div>
       <header className={commonColumnsStyles.AppHeader}>
         <p>Login Page</p>
-        <p>Logged user?: {localStorage.getItem("LoggedUser")}</p>
+        <p>Debugging: logged user = {localStorage.getItem("LoggedUser")}</p>
         <div>
           <input
             placeholder="User Name"
             value={username}
             onChange={(event) => setUsername(event.target.value)}
           ></input>
-          <Link to="/shopping">
-            {/* <button onClick={() => localStorage.setItem("LoggedUser", JSON.stringify(username))}> */}
-            <button onClick={signInUser}>Sign in</button>
-          </Link>
+          <button onClick={signInUser}>Sign in</button>
+
+          {/* <form onSubmit={signInUser}>
+            <label>User name</label>
+            <input onChange={(event) => setUsername(event.target.value)}></input>
+
+            <button type="submit"> Sign in</button>
+          </form> */}
         </div>
       </header>
     </div>
