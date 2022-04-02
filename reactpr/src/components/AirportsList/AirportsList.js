b3164d4bc6aaf9e6f5e763ef7eed08294dbd0a2c
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-04-02 14:15:43 +0200

    task 3 done

--- reactpr/src/components/AirportsList/AirportsList.js
+++ reactpr/src/components/AirportsList/AirportsList.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import commonColumnsStyles from "../../common/styles/Columns.module.scss";
 
 function ProductsList() {
+  let navigate = useNavigate();
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
         <p>Lista lotnisk</p>
         {JSON.parse(window.localStorage.getItem("airports"))?.map((airport) => (
-          <span key={airport.id}>
+          <span onClick={() => navigate("/airport/details/" + airport.id)} key={airport.id}>
             {" "}
             {airport.name} {airport.id}{" "}
           </span>
