b1659ec1624077165aecb38cc2bcc53c26bd4c3a
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-04-10 14:54:11 +0200

    task 2 done

---
+++ reactpr/src/redux/airports/reducer.js
@@ -0,0 +1,8 @@
+import { initialAirportsState } from "./initialState";
+
+export const airportReducer = (state = initialAirportsState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
