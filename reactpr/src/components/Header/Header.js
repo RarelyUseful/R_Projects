e2b8277867b20a3fa2b1f699d11041bbd11f501f
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-04-10 23:49:04 +0200

    task 8 done

--- reactpr/src/components/Header/Header.js
+++ reactpr/src/components/Header/Header.js
@@ -3,12 +3,14 @@ import styles from "../../common/styles/Headers.module.scss";
 import { Link } from "react-router-dom";
 import { Typography, Button } from "@mui/material";
 import { useDispatch } from "react-redux";
+import airports from "../../common/consts/airports";
 
 function Header() {
   const currentUser = JSON.parse(window.localStorage.getItem("user"));
   const dispatch = useDispatch();
-  const resetAirports = () => {return true};
-  // dispatch({ type: "SET_INITIAL_AIRPORTS_LIST", value: value });
+  const resetAirports = () => {
+    dispatch({ type: "SET_INITIAL_AIRPORTS_LIST", value: airports });
+  };
   return (
     <div className={styles.Wrapper}>
       <Typography sx={{ m: 2 }} variant="h5">
